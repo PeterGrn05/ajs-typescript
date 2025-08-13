fdbdcfea698706b001ba20420aa5827c3ba771c1
Горин Пётр <peter14102005@gmail.com>
2025-08-13 10:46:03 +0300

    cart changed

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,30 +1,23 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-    private _items: Buyable[] = [];
+  private _items: Buyable[] = [];
 
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
+  add(item: Buyable): void {
+    this._items.push(item);
+  }
 
-    get items(): Buyable[] {
-        return [...this._items];
-    }
+  get items(): Buyable[] {
+    return [...this._items];
+  }
 
-    price(discount: number): number {
-        let priceSum: number = 0;
-        this.items.forEach((e) => {
-            priceSum = priceSum + e.price
-        });
-        return discount < 1 && discount > 0 ? priceSum - priceSum * Number(discount) : priceSum;
-    }
+  price(discount: number): number {
+    const priceSum = this.items.reduce((sum, item) => sum + item.price, 0);
+    return discount > 0 && discount < 1 ? priceSum * (1 - discount) : priceSum;
+  }
 
-    delete(id: number): void {
-      this.items.forEach((e) => {
-        if (e.id === id) {
-          this._items.splice(this.items.indexOf(e), 1);
-        }
-      });
-    }
+  delete(id: number): void {
+    this._items = this._items.filter(item => item.id !== id);
+  }
 
 }
